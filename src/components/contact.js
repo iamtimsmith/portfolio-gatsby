ce73d9d4d81ab307ecb0db7788067dab9a74ad63
Tim  Smith <tim@iamtimsmith.com>
2018-04-19 23:09:18 -0500

    Added a message to display after form is submitted and clear form. Also added class to the index.js file so componentDidMount() can find window during the build process.

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,11 +4,19 @@ export default () => (
   <div className="column is-half">
     <iframe name="votar" style={{display:'none'}}></iframe>
     <form action="https://www.iamtimsmith.com/mailer.php" method="post" target="votar" id='contact'>
+      <p id='formResponse' style={{color:'green', opacity:'0', transition:'opacity .25s', textAlign:'center'}}>Thanks for reaching out to me. I'll be in contact as soon as possible!</p>
       <input type="text" name="name" placeholder="Name" required />
       <input type="text" name="email" placeholder="Email" required />
 			<p style={{display:'none'}}>Empty: <input type="text" name="url" /></p>
       <textarea name="message" placeholder="Your Message" required></textarea>
-      <input type="submit" name="submit" value="Hire Me!" />
+      <input type="submit" name="submit" value="Hire Me!" onClick={formSumbit}/>
     </form>
   </div>
 )
+
+const formSumbit = () => {
+  setTimeout( () => {
+    document.getElementById('contact').reset()
+    document.getElementById('formResponse').style.opacity = '1'
+  }, 1000)
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,40 +7,52 @@ import Item from '../components/portfolioItem'
 import OffCanvas from '../components/offCanvas'
 import Contact from '../components/contact'
 
-const IndexPage = ({data}) => (
-  <div id='home'>
-    <Header />
-    <Hero />
-    <div className="portfolio-items columns is-multiline" style={{marginTop:'0'}}>
-    {data.allMarkdownRemark.edges.map( ({node}) => (
-      <Item name={node.frontmatter.title} tags={node.frontmatter.tags} url={node.frontmatter.url} img={node.frontmatter.thumbnail} />
-    ))}
-    </div>
-    <section className="section columns is-centered" style={{paddingTop:'100px', paddingBottom:'100px'}}>
-      <Contact />
-    </section>
-    
-    <Footer />
-  </div>
-)
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  componentDidMount() {
+    window.onscroll = () => {
+      var top = window.pageYOffset
+      console.log(top)
+      var navbar = document.getElementById("navbar")
+      if(top < 650) {	
+        navbar.classList.add('clear')
+      }
+      else {
+        navbar.classList.remove('clear')
+      }
+  
+      if (document.getElementById('post') && top > 100) {
+        document.getElementById("post").classList.add("change")
+      } else {}
+    }
+  }
+  render() {
+    return (
+      <div id='home'>
+        <Header />
+        <Hero />
+        <div className="portfolio-items columns is-multiline" style={{marginTop:'0'}}>
+        {this.props.data.allMarkdownRemark.edges.map( ({node}) => (
+          <Item name={node.frontmatter.title} tags={node.frontmatter.tags} url={node.frontmatter.url} img={node.frontmatter.thumbnail} />
+        ))}
+        </div>
+        <section className="section columns is-centered" style={{paddingTop:'100px', paddingBottom:'100px'}}>
+          <Contact />
+        </section>
+        
+        <Footer />
+      </div>
+    )
+  }
+}
+
+
 
 export default IndexPage
 
-window.onscroll = () => {
-  var top = window.pageYOffset
-  console.log(top)
-  var navbar = document.getElementById("navbar")
-  if(top < 650) {	
-    navbar.classList.add('clear')
-  }
-  else {
-    navbar.classList.remove('clear')
-  }
 
-  if (document.getElementById('post') && top > 100) {
-    document.getElementById("post").classList.add("change")
-  } else {}
-};
 
 export const query = graphql`
   query ItemQuery {
